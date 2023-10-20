00ba406c19b2979a5d7aecb50855bfa80b2e3e4e
teayoungkim <aslkdj1@nextchip.com>
2023-10-20 19:27:36 +0900

    + array code

---
+++ array/array.h
@@ -0,0 +1,10 @@
+namespace ds
+{
+    class array
+    {
+    private:
+        int size;
+        int capacity;
+        int* data;
+    };
+} // namespace ds
---
+++ array/array_test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "array.h"
+
+int main()
+{
+    ds::array test;
+    std::cout << "hello world\n";
+    return 0;
+}
