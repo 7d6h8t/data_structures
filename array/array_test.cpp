e245e754fe725f81ecfd38f3f7cfb06151ea3cce
teayoungkim <aslkdj1@nextchip.com>
2023-10-23 19:12:49 +0900

    update array_test.cpp : testing template function

--- array/array_test.cpp
+++ array/array_test.cpp
@@ -1,8 +1,18 @@
 #include "array.h"
 #include <iostream>
 
+template <typename _Ty>
+void print(const _Ty arg) {
+    std::cout << arg << std::endl;
+}
+
+template <typename _Ty, typename... types>
+void print(const _Ty arg, types... args) {
+    std::cout << arg << ", " << std::flush;
+    print(args...);
+}
+
 int main() {
-  ds::array test;
-  std::cout << "hello world\n";
-  return 0;
+    print("hello world", 13, 12.5);
+    return 0;
 }
