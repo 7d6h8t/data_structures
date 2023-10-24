90bf97faeec8779a0a51c6b5dae0a6c4bfa919a3
teayoungkim <aslkdj1@nextchip.com>
2023-10-24 18:51:28 +0900

    + constructor -> array

--- array/array.h
+++ array/array.h
@@ -1,9 +1,24 @@
+#include <concepts>
 #include <cstdint>
-
 namespace ds {
-template <typename _Ty, std::size_t size>
+template <typename T, std::size_t size>
 class array {
 private:
-    _Ty elems[size];
+    void initialize() {}
+
+    template <typename... types>
+    void initialize(const T& arg, const types&... args) {
+        elems[size - sizeof...(args) - 1] = arg;
+        initialize(args...);
+    }
+
+public:
+    template <typename... types>
+    array(const types&... args) {
+        initialize(args...);
+    }
+
+private:
+    T elems[size];
 };
 } // namespace ds
--- array/array_test.cpp
+++ array/array_test.cpp
@@ -1,18 +1,34 @@
 #include "array.h"
+#include <array>
+#include <concepts>
 #include <iostream>
 
 template <typename _Ty>
-void print(const _Ty arg) {
-    std::cout << arg << std::endl;
+void print(const _Ty& arg) {
+    std::cout << arg << std::flush;
 }
 
 template <typename _Ty, typename... types>
-void print(const _Ty arg, types... args) {
-    std::cout << arg << ", " << std::flush;
+void print(const _Ty& arg, const types&... args) {
+    std::cout << arg << " " << std::flush;
     print(args...);
+    std::cout << std::endl;
+}
+
+template <typename T>
+concept Addable = requires(T a, T b) { a + b; };
+
+template <Addable... types>
+void add(const types&... args) {
+    std::cout << (... + args) << std::flush;
 }
 
 int main() {
-    print("hello world", 13, 12.5);
+    std::array<int32_t, 5> std_array_1;
+    std::array<int32_t, 5> std_array_2{1, 2, 3, 4, 5};
+
+    ds::array<int32_t, 5> ds_array_1;
+    ds::array<int32_t, 5> ds_array_2{10, 20, 30, 40, 50};
+
     return 0;
 }
