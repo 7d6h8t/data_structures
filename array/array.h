ec74270a62252a7b1a322d3aa18c361c4b212f4e
김태영 <7d6h8t@gmail.com>
2023-10-25 12:24:44 +0900

    + reuires

--- array/array.h
+++ array/array.h
@@ -5,6 +5,7 @@ template <typename T, std::size_t size>
 class array {
 public:
     template <typename... types>
+        requires(std::same_as<types, T> && ...)
     array(const types&... args) {
         std::size_t index = 0;
         ((elems[index++] = args), ...);
