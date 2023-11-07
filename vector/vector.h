5380dd1956b1931446e9e8c9f6fae77806286f54
김태영 <7d6h8t@gmail.com>
2023-11-07 20:21:35 +0900

    + constructor, destructor

--- vector/vector.h
+++ vector/vector.h
@@ -3,12 +3,31 @@
 namespace ds {
     template <typename T>
     class vector {
+    public:
+        using value_type = T;
+        using size_type = std::size_t;
 
     public:
         template <typename... types>
         requires(std::same_as<T, types> && ...)
-        vector(const types... arg) {}
+        vector(const types... args) {
+            size_ = sizeof...(args);
+            capaticy_ = size_;
+            value_ = new T[size_];
+        }
+
+        ~vector() {
+            size_ = 0;
+            capaticy_ = 0;
+            if (value_ != nullptr) {
+                delete[] value_;
+                value_ = nullptr;
+            }
+        }
 
     private:
+        size_type size_;
+        size_type capaticy_;
+        value_type* value_;
     };
 } // namespace ds
--- vector/vector_test.cpp
+++ vector/vector_test.cpp
@@ -1,3 +1,4 @@
+#include "vector.h"
 #include <iostream>
 #include <vector>
 
@@ -7,5 +8,8 @@ int main() {
     for (const auto& elem : vector_test)
         std::cout << elem << " " << std::flush;
     std::cout << std::endl;
+
+    ds::vector<int32_t> ds_vector1{1, 2, 3, 4, 5};
+
     return 0;
 }
