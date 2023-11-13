305629ab3e2b174c6c726f5b1bd38f34e32674bf
김태영 <7d6h8t@gmail.com>
2023-11-13 20:24:35 +0900

    + realloc()

--- vector/vector.h
+++ vector/vector.h
@@ -15,40 +15,53 @@ namespace ds {
         requires(std::same_as<T, types> && ...)
         vector(const types&... args) {
             size_ = sizeof...(args);
-            capaticy_ = size_;
-            value_ = new T[size_];
+            capacity_ = size_;
+            head_ = new T[size_];
         }
 
         ~vector() {
             size_ = 0;
-            capaticy_ = 0;
-            if (value_ != nullptr) {
-                delete[] value_;
-                value_ = nullptr;
+            capacity_ = 0;
+            if (head_ != nullptr) {
+                delete[] head_;
+                head_ = nullptr;
             }
         }
 
     public:
         reference at(const size_type pos) {
-            return *(value_ + pos);
+            return *(head_ + pos);
         }
 
         const_reference at(const size_type pos) const {
-            return *(value_ + pos);
+            return *(head_ + pos);
         }
 
         // capacity
         void reserve(const size_type new_cap) {
-            capaticy_ = new_cap;
+            capacity_ = new_cap;
         }
 
         size_type capacity() const {
-            return capaticy_;
+            return capacity_;
+        }
+
+    private:
+        void realloc() {
+            capacity_ *= 2;
+
+            value_type* temp = new value_type[capacity_];
+
+            for (size_type i = 0; i < size_; ++i)
+                temp[i] = head_[i];
+
+            delete[] head_;
+            head_ = temp;
         }
 
     private:
         size_type size_;
-        size_type capaticy_;
-        value_type* value_;
+        size_type capacity_;
+        value_type* head_;
     };
 } // namespace ds
