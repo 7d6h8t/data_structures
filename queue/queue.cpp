56e7d5334d7b2914eccedf98e042f5fe0de48966
김태영 <7d6h8t@gmail.com>
2023-12-04 14:47:58 +0900

    + ds::queue front(), back(), empty(), size(), push(), pop()

--- queue/queue.cpp
+++ queue/queue.cpp
@@ -17,6 +17,17 @@ int main() {
   std::cout << std::endl;
 
   ds::queue<int32_t> ds_queue;
+  ds_queue.push(0);
+  ds_queue.push(1);
+  ds_queue.push(2);
+  ds_queue.push(3);
+  ds_queue.push(4);
+
+  while (!ds_queue.empty()) {
+    std::cout << ds_queue.front() << " " << std::flush;
+    ds_queue.pop();
+  }
+  std::cout << std::endl;
 
   return 0;
 }
--- queue/queue.h
+++ queue/queue.h
@@ -3,6 +3,29 @@
 namespace ds {
 template <typename T, typename sequence = ds::list<T>>
 class queue final {
+ public:
+  using value_type = sequence::value_type;
+  using reference = sequence::reference;
+  using size_type = sequence::size_type;
+
+  queue() = default;
+  ~queue() = default;
+
+ public:
+  // element access
+  reference front() { return container_.front(); }
+
+  reference back() { return container_.back(); }
+
+  // capacity
+  bool empty() const { return container_.empty(); }
+
+  size_type size() const { return container_.size(); }
+
+  // modifiers
+  void push(const value_type& value) { container_.push_back(value); }
+
+  void pop() { container_.pop_front(); }
 
  private:
   sequence container_;
