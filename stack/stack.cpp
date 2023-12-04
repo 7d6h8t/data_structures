02b115efacf6718ee05121279d35493777f2404e
김태영 <7d6h8t@gmail.com>
2023-12-04 12:05:07 +0900

    + stack class

---
+++ stack/stack.cpp
@@ -0,0 +1,22 @@
+#include "stack.h"
+#include <iostream>
+#include <stack>
+
+int main() {
+  std::stack<int32_t> std_stack;
+  std_stack.push(0);
+  std_stack.push(1);
+  std_stack.push(2);
+  std_stack.push(3);
+  std_stack.push(4);
+
+  while (!std_stack.empty()) {
+    std::cout << std_stack.top() << std::flush;
+    std_stack.pop();
+  }
+  std::cout << std::endl;
+
+  ds::stack<int32_t> ds_stack;
+
+  return 0;
+}
---
+++ stack/stack.h
@@ -0,0 +1,6 @@
+#include "../list/list.h"
+
+namespace ds {
+template <typename T, typename sequnce = ds::list<T>>
+class stack final {};
+}  // namespace ds
