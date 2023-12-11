30b579c03571c1759404857a85d69399dfd2f508
김태영 <7d6h8t@gmail.com>
2023-12-11 15:42:25 +0900

    update node, const_iterator, iterator

--- list/list.h
+++ list/list.h
@@ -5,12 +5,120 @@ namespace ds {
   template <typename T>
   class list final {
   private:
-    class node final {};
+    class node final {
+    public:
+      node(const T &elem = T{}, node *next = nullptr, node *prev = nullptr)
+          : elem_(elem), next_(next), prev_(prev)
+      {
+      }
+
+      node(const T &&elem = T{}, node *next = nullptr, node *prev = nullptr)
+          : elem_(std::move(elem)), next_(next), prev_(prev)
+      {
+      }
+
+    private:
+      T elem_;
+      node *next_;
+      node *prev_;
+    };
 
   public:
-    class const_iterator {};
+    class const_iterator {
+    protected:
+      const_iterator(node *p) : curr_(p) {}
 
-    class iterator {};
+    public:
+      const_iterator() : curr_(nullptr) {}
+
+      const T &operator*() const
+      {
+        return curr_->elem_;
+      }
+
+      const_iterator &operator++()
+      {
+        curr_ = curr_->next_;
+        return *this;
+      }
+
+      const_iterator operator++(int)
+      {
+        const_iterator temp = *this;
+        ++(*this);
+        return temp;
+      }
+
+      const_iterator &operator--()
+      {
+        curr_ = curr_->prev_;
+        return *this;
+      }
+
+      const_iterator operator--(int)
+      {
+        const_iterator temp = *this;
+        --(*this);
+        return tmep;
+      }
+
+      bool operator==(const const_iterator &rhs) const
+      {
+        return (curr_ == rhs.curr_);
+      }
+
+      bool operator!=(const const_iterator &rhs) const
+      {
+        return (curr_ != rhs.curr_);
+      }
+
+    protected:
+      node *curr_;
+    };
+
+    class iterator : public const_iterator {
+    protected:
+      iterator(node *p) : const_iterator(p) {}
+
+    public:
+      iterator() {}
+
+      T &operator*()
+      {
+        return curr_->elem_;
+      }
+
+      const T &operator*() const
+      {
+        return const_iterator::operator*();
+      }
+
+      iterator &operator++()
+      {
+        curr_ = curr_->next_;
+        return *this;
+      }
+
+      iterator operator++(int)
+      {
+        iterator temp = *this;
+        ++(*this);
+        return temp;
+      }
+
+      iterator &operator--()
+      {
+        curr_ = curr_->prev_;
+        return *this;
+      }
+
+      iterator operator--(int)
+      {
+        iterator temp = *this;
+        --(*this);
+        return temp;
+      }
+    };
   };
 } // namespace ds
 #endif
