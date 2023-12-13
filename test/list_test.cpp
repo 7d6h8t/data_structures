52b40cc01ce9f01eac248fe7f0421a51bcaa53a0
김태영 <7d6h8t@gmail.com>
2023-12-13 16:20:39 +0900

    update list_test Suite(case)

--- test/list_test.cpp
+++ test/list_test.cpp
@@ -28,18 +28,19 @@ namespace ds {
       EXPECT_EQ(lst0_.size(), 0);
       EXPECT_TRUE(lst0_.empty());
 
-      lst0_.push_back(1);
+      EXPECT_NE(lst1_.size(), 0);
+      EXPECT_FALSE(lst1_.empty());
+    }
 
-      EXPECT_NE(lst0_.size(), 0);
-      EXPECT_FALSE(lst0_.empty());
-
+    TEST_F(listTest, ClearWorks)
+    {
       lst0_.clear();
-
       EXPECT_EQ(lst0_.size(), 0);
       EXPECT_TRUE(lst0_.empty());
 
-      EXPECT_NE(lst1_.size(), 0);
-      EXPECT_FALSE(lst1_.empty());
+      lst2_.clear();
+      EXPECT_EQ(lst2_.size(), 0);
+      EXPECT_TRUE(lst2_.empty());
     }
 
     TEST_F(listTest, PushWorks)
@@ -49,8 +50,70 @@ namespace ds {
       lst0_.push_back(2);
       lst0_.push_back(3);
       lst0_.push_back(4);
-
       EXPECT_EQ(lst0_.size(), 5);
+
+      lst1_.push_front(10);
+      lst1_.push_front(11);
+      lst1_.push_front(12);
+      EXPECT_EQ(lst1_.size(), 7);
+
+      lst2_.push_front(11);
+      lst2_.push_front(10);
+      lst2_.push_back(5);
+      lst2_.push_back(6);
+      EXPECT_EQ(lst2_.size(), 9);
+      EXPECT_EQ(*(++lst2_.begin()), 11);
+      EXPECT_EQ(lst2_.front(), 10);
+      EXPECT_EQ(lst2_.back(), 6);
+
+      lst3_.push_back("hello");
+      lst3_.push_back("world");
+      lst3_.push_back("!");
+      EXPECT_EQ(lst3_.size(), 3);
+      EXPECT_EQ(*(--lst3_.end()), "!");
+    }
+
+    TEST_F(listTest, PopWorks)
+    {
+      lst1_.pop_front();
+      lst1_.pop_back();
+      EXPECT_EQ(lst1_.size(), 2);
+      EXPECT_EQ(lst1_.back(), 2);
+
+      lst2_.pop_back();
+      lst2_.pop_back();
+      lst2_.pop_back();
+      EXPECT_EQ(lst2_.size(), 2);
+      EXPECT_EQ(lst2_.back(), 1);
+    }
+
+    TEST_F(listTest, InsertWorks)
+    {
+      lst2_.insert(lst2_.end(), 5);
+      for (auto itr = lst2_.begin(); itr != lst2_.end(); ++itr) {
+        if (*itr == 1) {
+          lst2_.insert(itr, 10);
+          break;
+        }
+      }
+      EXPECT_EQ(lst2_.size(), 7);
+      EXPECT_EQ(lst2_.front(), 0);
+      EXPECT_EQ(*(++lst2_.begin()), 10);
+    }
+
+    TEST_F(listTest, EraseWorks)
+    {
+      lst1_.erase(lst1_.begin());
+      for (auto itr = lst1_.begin(); itr != lst1_.end();) {
+        if (*itr % 2 == 0)
+          itr = lst1_.erase(itr);
+        else
+          ++itr;
+      }
+
+      EXPECT_EQ(lst1_.size(), 2);
+      EXPECT_EQ(lst1_.back(), 3);
+      EXPECT_EQ(*(++lst1_.begin()), 3);
     }
 
   } // namespace
